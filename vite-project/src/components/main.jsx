740aaf2e1a28e2ae7607ea0879ed41978712aaa8
PieStw <piero.stalteri@icloud.com>
2024-11-29 16:02:56 +0100

    aggiunti tags come bottoni nella parte superiore

--- vite-project/src/components/main.jsx
+++ vite-project/src/components/main.jsx
@@ -3,16 +3,36 @@ import Card from "./card";
 import "./main.css";
 
 export default function Main({ posts }) {
+  const tags = [];
+
+  posts.forEach((post) => {
+    post.tags.forEach((tag) => {
+      if (!tags.includes(tag)) {
+        tags.push(tag);
+      }
+    });
+  });
+
   return (
     <>
       <main className="main">
-        {posts
-          .filter((post) => post.published)
-          .map((post, index) => (
-            <div key={index} className="main-div">
-              <Card post={post} />
+        <div className="filter">
+          {tags.map((tag, index) => (
+            <div key={index} className="filter-div">
+              <button className="btn btn-primary">{tag}</button>
             </div>
           ))}
+        </div>
+
+        <div className="main-div">
+          {posts
+            .filter((post) => post.published)
+            .map((post, index) => (
+              <div key={index} className="main-div">
+                <Card post={post} />
+              </div>
+            ))}
+        </div>
       </main>
     </>
   );
